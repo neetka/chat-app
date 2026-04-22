f17f16c0d55ca678dee21d3e20d4f91dc1e27eeb
nk <nikksacc@gmail.com>
2026-04-22 20:54:19 +0530

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,9 +23,7 @@ app.use(express.json({ limit: "10mb" })); // increases the payload size limit
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production"
-      ? process.env.FRONTEND_URL
-      : /^http:\/\/localhost:\d+$/,
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://yapp-27.vercel.app"],
     credentials: true,
   })
 );
